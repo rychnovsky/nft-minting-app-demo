7db464887b585300dd4b8042740ea59462028276
Vojtěch Rychnovský <vojtech.rychnovsky@gmail.com>
2024-04-21 19:46:02 +0200

    feat: repo setup with frontend and hardhat package

---
+++ packages/hardhat/contracts/NFTContract.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+
+contract NFTContract is ERC721, ERC721Enumerable {
+    constructor() ERC721('TallinTestNFT', 'TTN') {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return 'https://d/';
+    }
+
+    function safeMint(address to) public {
+        uint256 tokenId = totalSupply();
+        _safeMint(to, tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public pure override returns (string memory) {
+        return string(abi.encodePacked(_baseURI()));
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ packages/hardhat/test/NFTContract.test.ts
@@ -0,0 +1,43 @@
+import { expect } from 'chai'
+import hre from 'hardhat'
+
+import { NFTContract } from '../typechain-types'
+
+describe('NFTContract', function () {
+  let contract: NFTContract
+
+  beforeEach(async function () {
+    contract = await hre.ethers.deployContract(
+      'NFTContract',
+      [], // constructor arguments
+      {} // overrides
+    )
+  })
+
+  describe('Deployment', function () {
+    it('Should deploy the contract', async function () {
+      expect(await contract.getAddress()).to.not.be.undefined
+    })
+    it('Should have no initial supply', async function () {
+      expect(await contract.totalSupply()).to.eq(0)
+    })
+  })
+
+  describe('safeMint()', function () {
+    it('Should mint the right token to the right owner', async function () {
+      // arrange
+      const [owner] = await hre.ethers.getSigners()
+      const totalSupply = await contract.totalSupply()
+      expect(totalSupply).to.equal(0)
+
+      // act
+      const action = await contract.safeMint(owner.address)
+      expect(action).not.to.be.reverted
+
+      // assert
+      const newTotalSupply = await contract.totalSupply()
+      expect(newTotalSupply).to.equal(1)
+      console.log('token URI', await contract.tokenURI(1))
+    })
+  })
+})
